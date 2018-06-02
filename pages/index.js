a9e7fe2672887fdef2b3de8f9bd408e37eb8500c
ashishgaikwad <ashish.gd@gmail.com>
2018-06-02 13:07:54 +0530

    backup

---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const PostLink = ({show}) => (
+    <li key={show.id}>
+        <Link as={`/post/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+        </Link>
+        <style jsx>{`
+            li {
+                list-style: none;
+                maring: 5px 0;
+            }
+
+            a {
+                text-decoration: none;
+                color: blue;
+                font-family: "Arial";
+            }
+
+            a:hover {
+                opacity: 0.6;
+            }
+        `}
+        </style>
+    </li>
+)
+
+const Index = (props) => (
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(({show}) => (
+              <PostLink show={show}/>
+            ))}
+        </ul>
+        <style jsx>{`
+            
+            h1, a {
+                font-family: "Arial";
+            }
+
+            ul {
+                padding: 0;
+            }
+        `}
+        </style>
+    </Layout>
+);
+
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows:data
+    }
+}
+
+export default Index;
